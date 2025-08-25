10e133bafae837e6e1402a161e2fce2ecd6d46cd
Heitor Belem <32557513+heitorlbelem@users.noreply.github.com>
2025-08-25 12:06:30 +0000

    testando retorno de log para `/status`

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,23 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log('Credenciais do Banco: ', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  })
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error
   } finally {
-    await client.end();
+    await client.end()
+    throw "aaaa";
   }
 }
 
