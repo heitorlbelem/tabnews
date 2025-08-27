6ef066d2efdcd3f7d5cb98509927fef066fec5bb
Heitor Belem <32557513+heitorlbelem@users.noreply.github.com>
2025-08-27 17:57:20 +0000

    retorna código para estado inicial com bug no request

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -16,6 +16,7 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -24,6 +25,8 @@ export default async function migrations(request, response) {
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
