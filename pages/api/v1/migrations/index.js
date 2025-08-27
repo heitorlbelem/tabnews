d66b98b5216563b52b02d96e422a6da24b12685a
Heitor Belem <32557513+heitorlbelem@users.noreply.github.com>
2025-08-27 04:46:03 +0000

    adiciona endpoint `/api/v1/migrations`

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,33 @@
+import database from "infra/database";
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+  if (request.method === "POST") {
+    const appliedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    if (appliedMigrations.length > 0) {
+      return response.status(201).json(appliedMigrations);
+    }
+    return response.status(200).json(appliedMigrations);
+  }
+  await dbClient.end();
+  response.status(405).end();
+}
