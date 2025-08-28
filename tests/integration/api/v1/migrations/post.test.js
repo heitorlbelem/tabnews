2ff6555ed6ea2d1f8eb6c71c187ab40f19f4917f
Heitor Belem <32557513+heitorlbelem@users.noreply.github.com>
2025-08-28 03:03:55 +0000

    corrige erro no endpoint `/migrations` relacionado a pool de conexões com DB

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -25,3 +25,12 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
 });
+
+test("Not allowed HTTP Method should return 405", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  const responseBody = await response.json();
+  expect(response.status).toBe(405);
+  expect(responseBody.error).toEqual(`Method 'DELETE' not allowed`);
+});
